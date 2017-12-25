5dc1df30439f416477fafe9cb2e1ebd138c7e2c2
Andrey Ermilov <andrerm@ya.ru>
2017-12-25 22:34:09 +0300

    Fixes git clone command

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone git@github.com:Otus-DevOps-2017-11/AndreyErmilov_infra.git
+git clone -b Infra-2 https://github.com/Otus-DevOps-2017-11/AndreyErmilov_infra.git
 cd AndreyErmilov_infra
 sudo scripts/install_ruby.sh
 sudo scripts/install_mongodb.sh
