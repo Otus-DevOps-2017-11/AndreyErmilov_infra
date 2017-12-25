e9fb6542536d37752fdf3768e46c8b25c2a22de0
Andrey Ermilov <andrerm@ya.ru>
2017-12-25 23:49:02 +0300

    Adds to startup.sh all action

--- startup.sh
+++ startup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-git clone -b Infra-2 https://github.com/Otus-DevOps-2017-11/AndreyErmilov_infra.git
-cd AndreyErmilov_infra
-sudo install_ruby.sh
-sudo install_mongodb.sh
-sudo deploy.sh
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+git clone https://github.com/Otus-DevOps-2017-11/reddit.git
+cd reddit && bundle install
+puma -d
